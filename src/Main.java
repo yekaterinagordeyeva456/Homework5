4f5c720ad6c4c3d0c88f5543dd81bbb209eb9845
yekaterinagordeyeva456 <yges1979@gmail.com>
2023-02-17 00:05:07 +0600

    HW for checking

--- src/Main.java
+++ src/Main.java
@@ -23,53 +23,55 @@ public class Main {
         }
         // Задание 3
         System.out.println("Задание 3");
-        int year = 401;
-        if ((year % (year / 4)) == 0) {
+        int year = 16100;
+        if (year % 4 == 0 && year % 400 ==0) {
             System.out.println(year + " - год является високосным");
-        } else {
-            System.out.println(year + " - год неявляется високосным");
-        }
-        // Задание 4
-        System.out.println("Задание 4");
-        int deliveryDistance = 100;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: 1 срок доставки.");
-        }
-        if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2 срок доставки.");
-        }
-        if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            System.out.println("Потребуется дней: 3 срок доставки.");
-        } else {
-            System.out.println("Пставка не выполняется");
-        }
-        // Задание 5
-        System.out.println("Задание 5");
-        int monthNumber = 16;
+        }else if (year % 100 == 0) {
+                System.out.println(year + " - год неявляется високосным");
+            } else {
+                System.out.println(year + " - год неявляется високосным");
+            }
+            // Задание 4
+            System.out.println("Задание 4");
+            int deliveryDistance = 100;
+            if (deliveryDistance < 20) {
+                System.out.println("Потребуется дней: 1 срок доставки.");
+            }
+            if (deliveryDistance >= 20 && deliveryDistance < 60) {
+                System.out.println("Потребуется дней: 2 срок доставки.");
+            }
+            if (deliveryDistance >= 60 && deliveryDistance < 100) {
+                System.out.println("Потребуется дней: 3 срок доставки.");
+            } else {
+                System.out.println("Пставка не выполняется");
+            }
+            // Задание 5
+            System.out.println("Задание 5");
+            int monthNumber = 16;
 
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("сезон Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("сезон Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("сезон Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("сезон Осень");
-                break;
-            default:
-                System.out.println("Такого сезона не существует");
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("сезон Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("сезон Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("сезон Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("сезон Осень");
+                    break;
+                default:
+                    System.out.println("Такого сезона не существует");
+            }
         }
     }
-}
